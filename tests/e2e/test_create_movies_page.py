a0b6cf8aaadf7a2fd0155ddc211c824a8579d787
sumiverma <sumistargazer@gmail.com>
2022-10-25 21:07:08 -0400

    added testing for create movie

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,8 @@
 # TODO: Feature 2
+from flask.testing import FlaskClient
+
+def test_create_page(test_app: FlaskClient):
+    response = test_app.get('/movies/new')
+    response_data = response.data
+
+    assert b'<p class="mb-3">Create a movie rating below</p>' in response_data
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,29 @@
 # TODO: Feature 2
+from src.models.movie import Movie
+from src.repositories.movie_repository import get_movie_repository
+from app import app
+#import pytest
+movie_repository = get_movie_repository()
+
+# test that feature works 
+def test_movie_created_successfully():
+    test_app = app.test_client()
+    #this should make the movie
+    movie_repository.create_movie('Psycho', 'Ur Mom', 5)
+    movie = Movie('Psycho', 'Ur Mom', 5)
+
+    assert type(movie) == Movie
+    assert movie.title == 'Psycho'
+    assert movie.director == 'Ur Mom'
+    assert movie.rating == 5
+
+# test that feature doesn't work
+def test_movie_not_created_successfully():
+    test_app = app.test_client()
+
+    movie_repository.create_movie('The Godmother', 'Bob Ricky', 3)
+    movie = Movie('The Godmother', 'Bob Ricky', 3)
+    
+    assert not movie.title == 'The Godmothers'
+    assert not movie.director == 'Bob Rickyo'
+    assert not movie.rating == 4
