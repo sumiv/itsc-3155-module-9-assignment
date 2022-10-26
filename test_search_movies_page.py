214dba925b49e03b309c3bd4aa2f19f29dd7dcb9
hsammon2 <93053380+hsammon2@users.noreply.github.com>
2022-10-25 20:19:45 -0400

    Delete test_search_movies_page.py

--- test_search_movies_page.py
+++
@@ -1,9 +0,0 @@
-# TODO: Feature 3
-from flask.testing import FlaskClient
-
-
-def test_home_page(test_app: FlaskClient):
-    response = test_app.get('/movies/search')
-    response_data = response.data
-
-    assert b'<p class="mb-3">Search for a movie rating below</p>' in response_data
